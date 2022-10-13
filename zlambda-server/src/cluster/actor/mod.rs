3a45cbb9209bd80e4cd3d7b363ab26a0981878db
Justus Flerlage <justus@sutsuj.com>
2022-10-14 01:07:13 +0200

    Remove old code

--- zlambda-server/src/cluster/actor/mod.rs
+++ zlambda-server/src/cluster/actor/mod.rs
@@ -1,11 +1,7 @@
-//mod follower;
-//mod leader;
 mod node;
 mod packet_reader;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//pub(self) use follower::*;
-//pub(self) use leader::*;
 pub use node::*;
 pub(self) use packet_reader::*;
