116be0b7c3b25a926ee746c62c46d38dd115495b
Justus Flerlage <justus@sutsuj.com>
2023-01-07 23:59:50 +0100

    Current work status

--- zlambda-common/src/message/client_to_node.rs
+++ zlambda-common/src/message/client_to_node.rs
@@ -8,6 +8,106 @@ use serde::{Deserialize, Serialize};
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ClientToNodeInitializeRequestMessage {}
+
+impl ClientToNodeInitializeRequestMessage {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ClientToNodeAppendMessage {
+    module_id: ModuleId,
+    bytes: Bytes,
+}
+
+impl ClientToNodeAppendMessage {
+    pub fn new(
+        module_id: ModuleId,
+        bytes: Bytes,
+    ) -> Self {
+        Self {
+            module_id,
+            bytes,
+        }
+    }
+
+    pub fn module_id(&self) -> ModuleId {
+        self.module_id
+    }
+
+    pub fn bytes(&self) -> &Bytes {
+        &self.bytes
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ClientToNodeLoadRequestMessage {
+    module_id: ModuleId,
+}
+
+impl ClientToNodeLoadRequestMessage {
+    pub fn new(module_id: ModuleId) -> Self {
+        Self {
+            module_id,
+        }
+    }
+
+    pub fn module_id(&self) -> ModuleId {
+        self.module_id
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ClientToNodeDispatchRequestMessage {
+    dispatch_id: DispatchId,
+    module_id: ModuleId,
+    payload: Vec<u8>,
+    node_id: Option<NodeId>,
+}
+
+impl ClientToNodeDispatchRequestMessage {
+    pub fn new(
+        dispatch_id: DispatchId,
+        module_id: ModuleId,
+        payload: Vec<u8>,
+        node_id: Option<NodeId>,
+    ) -> Self {
+        Self {
+            dispatch_id,
+            module_id,
+            payload,
+            node_id,
+        }
+    }
+
+    pub fn dispatch_id(&self) -> DispatchId  {
+        self.dispatch_id
+    }
+
+    pub fn module_id(&self) -> ModuleId {
+        self.module_id
+    }
+
+    pub fn payload(&self) -> &Vec<u8> {
+        &self.payload
+    }
+
+    pub fn node_id(&self) -> Option<NodeId> {
+        self.node_id
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum ClientToNodeMessage {
     InitializeRequest,
     Append {
