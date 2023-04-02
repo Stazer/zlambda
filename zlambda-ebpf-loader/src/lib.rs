9522db70245fa2a9f78079356bd021c5159f4947
Justus Flerlage <justus@sutsuj.com>
2023-04-02 23:39:52 +0200

    Current work status

--- zlambda-ebpf-loader/src/lib.rs
+++ zlambda-ebpf-loader/src/lib.rs
@@ -1,17 +1,19 @@
 use zlambda_core::common::async_trait;
+use zlambda_core::common::bytes::Bytes;
 use zlambda_core::common::module::{Module};
 use serde::{Deserialize, Serialize};
 use zlambda_core::server::{
     ServerModule, ServerModuleNotificationEventInput, ServerModuleNotificationEventOutput,
+    ServerModuleCommitEventInput, ServerModuleCommitEventOutput,
 };
+use aya::Bpf;
+use aya::programs::Xdp;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Deserialize, Serialize)]
 enum LogEntryData {
-    Prepare(usize),
-    Append(usize),
-    Load(usize),
+    Load(Bytes),
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -24,7 +26,8 @@ struct NotificationHeader {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Default, Debug)]
-pub struct EbpfLoader {}
+pub struct EbpfLoader {
+}
 
 #[async_trait]
 impl Module for EbpfLoader {}
@@ -35,5 +38,12 @@ impl ServerModule for EbpfLoader {
         &self,
         _input: ServerModuleNotificationEventInput,
     ) -> ServerModuleNotificationEventOutput {
+        println!("{:?}", _input);
+    }
+
+    async fn on_commit(
+        &self,
+        _input: ServerModuleCommitEventInput,
+    ) -> ServerModuleCommitEventOutput {
     }
 }
